1537943b433cfeaec0c41eca9cd605471a358480
LucasGoulart24 <Goulart7720@gmail.com>
2025-03-12 16:39:50 -0300

    filme

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 
 // Icones
 import { CiSearch } from "react-icons/ci";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 function App() {
   return (
@@ -16,7 +19,13 @@ function App() {
           <input type="text" placeholder="Digite nome do filme"></input>
           <button><CiSearch/></button>
         </div>
+        <div className="Redes">
+          <a href="#"><FaFacebookF/></a>
+          <a href="#"><FaInstagram/></a>
+          <a href="#"><FaXTwitter/></a>
+        </div>
       </div>
+      <div className="Principal"></div>
     </div>
   );
 }
