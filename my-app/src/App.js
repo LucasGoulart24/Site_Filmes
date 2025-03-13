b96d3298b484b4258b196b2c1dfe0d8d968c4c78
LucasGoulart24 <Goulart7720@gmail.com>
2025-03-13 18:29:36 -0300

    filme

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 import { CiSearch } from "react-icons/ci";
 import { FaBell } from "react-icons/fa";
 import { FaRegUser } from "react-icons/fa";
-
+import { FaList } from "react-icons/fa";
+import { FaCaretRight } from "react-icons/fa";
 
 function App() {
   return (
@@ -19,12 +20,24 @@ function App() {
           <input type="text" placeholder="Digite nome do filme"></input>
           <button><CiSearch/></button>
         </div>
-        <div className="Redes">
+        <div className="Icones">
           <a href="#"><FaBell/></a>
+          <a href="#"><FaList/></a>
           <a href="#" id="login"><FaRegUser/></a>
         </div>
       </div>
-      <div className="Principal"></div>
+      <div className="Principal">
+        <h1>Furiosa: Uma Saga Mad Max</h1>
+        <div className="Barra">
+          <span>16</span>
+          <p>2024</p>
+          <p>Ação/Aventura</p>
+          <p>2h 28m</p>
+        </div>
+        <div className="Barra">
+          <button><FaCaretRight/> Assistir</button>
+        </div>
+      </div>
     </div>
   );
 }
