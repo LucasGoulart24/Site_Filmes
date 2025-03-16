1e29ece85a3b6060007c419a59a23ccd9946662d
LucasGoulart24 <Goulart7720@gmail.com>
2025-03-15 23:35:25 -0300

    filme

--- my-app/src/Components/Filmes.js
+++ my-app/src/Components/Filmes.js
@@ -3,6 +3,10 @@ import Capa2 from "./Img/Filme2.jpg";
 import Capa3 from "./Img/Filme3.jpg";
 import Capa4 from "./Img/Filme4.jpeg";
 import Capa5 from "./Img/Filme5.jpeg";
+import Capa6 from "./Img/Filme6.jpeg";
+import Capa7 from "./Img/Filme7.jpeg";
+import Capa8 from "./Img/Filme8.jpeg";
+import Capa9 from "./Img/Filme9.jpeg";
 import "./Styles/Style.css";
 
 function Filmes() {
@@ -39,40 +43,40 @@ function Filmes() {
             <div className="Card">
                 <img src={Capa5}></img>
                 <div className="Textos">
-                    <p>2020</p>
-                    <p>Ação/Fantasia</p>
+                    <p>2024</p>
+                    <p>Terror/Mistério</p>
                 </div>
             </div>
             <div className="Card">
-                <img src={Capa5}></img>
+                <img src={Capa6}></img>
                 <div className="Textos">
-                    <p>2020</p>
-                    <p>Ação/Fantasia</p>
+                    <p>2024</p>
+                    <p>Ação/Ficção científica</p>
                 </div>
             </div>
             <div className="Card">
-                <img src={Capa5}></img>
+                <img src={Capa7}></img>
                 <div className="Textos">
-                    <p>2020</p>
-                    <p>Ação/Fantasia</p>
+                    <p>2024</p>
+                    <p>Terror/Suspense</p>
                 </div>
             </div>
             <div className="Card">
-                <img src={Capa5}></img>
+                <img src={Capa8}></img>
                 <div className="Textos">
-                    <p>2020</p>
-                    <p>Ação/Fantasia</p>
+                    <p>2024</p>
+                    <p>Ação/Ficção científica</p>
                 </div>
             </div>
             <div className="Card">
-                <img src={Capa5}></img>
+                <img src={Capa9}></img>
                 <div className="Textos">
-                    <p>2020</p>
-                    <p>Ação/Fantasia</p>
+                    <p>2024</p>
+                    <p>Ação/Ficção científica</p>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Filmes;
