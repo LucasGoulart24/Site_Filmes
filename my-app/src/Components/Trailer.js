2300afd35838ee1dae61ab49c6f5b26dcba25bb9
LucasGoulart24 <Goulart7720@gmail.com>
2025-03-26 15:05:55 -0300

    filme

--- my-app/src/Components/Trailer.js
+++ my-app/src/Components/Trailer.js
@@ -3,7 +3,10 @@ import "./Styles/Style.css";
 function Trailer() {
     return(
         <div className="Trailer">
-            <div className="Poster"></div>
+            <div className="Poster">
+                <div className="Titulo"></div>
+                <div className="Imagem"></div>
+            </div>
         </div>
     );
 }
